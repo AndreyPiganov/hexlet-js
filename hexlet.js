bcebcfb567c8e193bd47e68376c2e2094ba0d608
AndreyPiganov <andrey.piganov@mail.ru>
2022-11-16 10:32:43 +0300

    new content in files

--- hexlet.js
+++ hexlet.js
@@ -1,1 +1,8 @@
+import _ from "lodash";
+let c = Math.round(5.34);
+let d = _.intersection([2,1],[2,3]);
+let e = _.capitalize("hello");
+console.log(e);
+console.log(d);
+console.log(c);
 console.log('Hello Hexlet');
