5c84cbe56fee3cc5283f252a5a850e36a1578999
AndreyPiganov <andrey.piganov@mail.ru>
2022-11-16 12:15:51 +0300

    add new filse

--- hexlet.js
+++ hexlet.js
@@ -1,9 +1,10 @@
-import _ from "lodash";
-let c = Math.round(5.34);
-let d = _.intersection([2, 1], [2, 3]);
-let e = _.capitalize("hello");
+import _ from 'lodash';
+
+const c = Math.round(5.34);
+const d = _.intersection([2, 1], [2, 3]);
+const e = _.capitalize('hello');
 console.log(e);
 console.log(d);
 console.log(c);
-console.log("Hello Hexlet");
-console.log(_.last(["one", "two"]));
+console.log('Hello Hexlet');
+console.log(_.last(['one', 'two']));
